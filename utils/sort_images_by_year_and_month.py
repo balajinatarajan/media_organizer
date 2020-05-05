b83246562a61c80debad6d721d5474b6db6e54bf
Balaji Natarajan <code.balaji@gmail.com>
2020-05-05 10:58:12 -0400

    Updated exception handling
    Updated exception handling to also include exif errors, added a logic to run the script in batches

--- utils/sort_images_by_year_and_month.py
+++ utils/sort_images_by_year_and_month.py
@@ -7,40 +7,43 @@ import sys
 
 imageExtensions = [".JPG", ".jpg", ".jpeg", ".JPEG", ".png", ".PNG"]
 videoExtensions = [".mp4", ".MP4", ".mov", ".MOV", ".m4v", ".M4V"]
-mediaRoot = "/Users/bala/tmp/"
+mediaRoot = "F:/ALL_MEDIA/Pictures/"
 
 
 def getCreationDate(fileName):
-    image = Image.open(fileName)
-    image.verify()
-    return image._getexif()[36867]
+	image = Image.open(fileName)
+	image.verify()
+	return image._getexif()[36867]
 
 
 def createDateFoldersAndMoveMedia(dateString, fileName, fullFileName):
-    try:
-        year = re.findall('(\d{4}):\d{2}:\d{2}', dateString)
-        month = re.findall('\d{4}:(\d{2}):\d{2}', dateString)
-        newDir = mediaRoot + year[0] + "/" + month[0]
-        os.makedirs(newDir, exist_ok=True)
-        move(fullFileName, os.path.join(newDir, fileName))
-        return True
-    except:
-        print("Error moving " + fullFileName + ".." + sys.exc_info()[0])
-        return False
+	year = re.findall('(\d{4}):\d{2}:\d{2}', dateString)
+	month = re.findall('\d{4}:(\d{2}):\d{2}', dateString)
+	newDir = mediaRoot + year[0] + "/" + month[0]
+	os.makedirs(newDir, exist_ok=True)
+	move(fullFileName, os.path.join(newDir, fileName))
+	return True
 
 
 def organizeImagesByDateFolders(srcdir):
-    srcFiles = os.listdir(srcdir)
-    for fileName in srcFiles:
-        fullFileName = os.path.join(srcdir, fileName)
-        if (not os.path.isdir(fullFileName)):
-            if any(s in fileName for s in imageExtensions):
-                imageCreationDate = getCreationDate(fullFileName)
-                moveResult = createDateFoldersAndMoveMedia(
-                    imageCreationDate, fileName, fullFileName)
-                if (not moveResult):
-                    print(fileName)
+	srcFiles = os.listdir(srcdir)
+	counter = 0
+	errorCount = 0
+	for fileName in srcFiles:     
+		fullFileName = os.path.join(srcdir, fileName)
+		if (not os.path.isdir(fullFileName)):
+			if any(s in fileName for s in imageExtensions) and not fileName.startswith('.'):
+				try:
+					counter = counter + 1
+					imageCreationDate = getCreationDate(fullFileName)
+					moveResult = createDateFoldersAndMoveMedia(imageCreationDate, fileName, fullFileName)
+				except Exception as e:
+					errorCount = errorCount + 1
+					#print("Error copying file " + fileName + " with error " + e)
+		if (counter == 20000):
+			break
+	print("Total number of errors: " + str(errorCount))
 
 
 organizeImagesByDateFolders(
-    "/Users/bala/tech/workspace/python/filesorter/test")
+	"F:/ALL_MEDIA")
