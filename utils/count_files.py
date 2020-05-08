d8a28bb70ee78b001be11a5ccb2ba054571d9a64
Bala Natarajan <code.balaji@gmail.com>
2020-05-08 12:14:32 -0400

    Added Recursive media file count

---
+++ utils/count_files.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+otherFileTypes = []
+
+imageExtensions = [".JPG", ".jpg", ".jpeg", ".JPEG", ".png", ".PNG"]
+videoExtensions = [".mp4", ".MP4", ".mov", ".MOV", ".m4v", ".M4V"]
+
+
+def countFilesRecursively(srcdir, count):
+    srcFiles = os.listdir(srcdir)
+    for fileName in srcFiles:
+        fullFileName = os.path.join(srcdir, fileName)
+        if (os.path.isdir(fullFileName)):
+            count = countFilesRecursively(
+                fullFileName, count)
+        else:
+            if not fileName.startswith('.') and (any(s in fileName for s in imageExtensions) or any(s in fileName for s in videoExtensions)):
+                count = count + 1
+                extn = ""
+            try:
+                extn = fileName.rsplit(".", 1)[1]
+            except:
+                pass
+            if extn not in otherFileTypes:
+                otherFileTypes.append(extn)
+    return count
+
+
+src = "/Volumes/MONTUX-2TB"
+count = countFilesRecursively(src, 0)
+print("Total media: " + str(count))
+print(otherFileTypes)
