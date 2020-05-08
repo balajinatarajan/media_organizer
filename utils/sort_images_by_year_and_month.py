595268c5103cc18ab0f826aa422390dd88bda4ad
Bala Natarajan <code.balaji@gmail.com>
2020-05-07 21:15:49 -0400

    Added Support for Video
    refactored code to support video using hachoir and subprocess

--- utils/sort_images_by_year_and_month.py
+++ utils/sort_images_by_year_and_month.py
@@ -4,46 +4,74 @@ import re
 from shutil import move
 from PIL import Image
 import sys
+import subprocess
+from enum import Enum
 
 imageExtensions = [".JPG", ".jpg", ".jpeg", ".JPEG", ".png", ".PNG"]
 videoExtensions = [".mp4", ".MP4", ".mov", ".MOV", ".m4v", ".M4V"]
-mediaRoot = "F:/ALL_MEDIA/Pictures/"
+
+
+class MediaType(Enum):
+    IMAGE = 1
+    VIDEO = 2
+
+
+mediaRoot = "F:/ALL_MEDIA/Video/"
 
 
 def getCreationDate(fileName):
-	image = Image.open(fileName)
-	image.verify()
-	return image._getexif()[36867]
+    if any(s in fileName for s in imageExtensions):
+        image = Image.open(fileName)
+        image.verify()
+        return image._getexif()[36867]
+    else:
+        if any(s in fileName for s in videoExtensions):
+            result = subprocess.Popen(['hachoir-metadata', fileName, '--raw', '--level=6'],
+                                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            results = result.stdout.read().decode('utf-8').split('\n')
+            creation_date = ''
+            for item in results:
+                if item.startswith('- creation_date: '):
+                    creation_date = item.lstrip('- creation_date: ')
+            return creation_date
+    return ""
 
 
 def createDateFoldersAndMoveMedia(dateString, fileName, fullFileName):
-	year = re.findall('(\d{4}):\d{2}:\d{2}', dateString)
-	month = re.findall('\d{4}:(\d{2}):\d{2}', dateString)
-	newDir = mediaRoot + year[0] + "/" + month[0]
-	os.makedirs(newDir, exist_ok=True)
-	move(fullFileName, os.path.join(newDir, fileName))
-	return True
+    year = re.findall('(\d{4}):\d{2}:\d{2}', dateString)
+    if(len(year) == 0):
+        year = re.findall('(\d{4})-\d{2}-\d{2}', dateString)
+    month = re.findall('\d{4}:(\d{2}):\d{2}', dateString)
+    if(len(month) == 0):
+        month = re.findall('\d{4}-(\d{2})-\d{2}', dateString)
+    newDir = mediaRoot + year[0] + "/" + month[0]
+    os.makedirs(newDir, exist_ok=True)
+    move(fullFileName, os.path.join(newDir, fileName))
+    return True
 
 
-def organizeImagesByDateFolders(srcdir):
-	srcFiles = os.listdir(srcdir)
-	counter = 0
-	errorCount = 0
-	for fileName in srcFiles:     
-		fullFileName = os.path.join(srcdir, fileName)
-		if (not os.path.isdir(fullFileName)):
-			if any(s in fileName for s in imageExtensions) and not fileName.startswith('.'):
-				try:
-					counter = counter + 1
-					imageCreationDate = getCreationDate(fullFileName)
-					moveResult = createDateFoldersAndMoveMedia(imageCreationDate, fileName, fullFileName)
-				except Exception as e:
-					errorCount = errorCount + 1
-					#print("Error copying file " + fileName + " with error " + e)
-		if (counter == 20000):
-			break
-	print("Total number of errors: " + str(errorCount))
+def organizeMediaByDateFolders(srcdir, mediaType):
+    srcFiles = os.listdir(srcdir)
+    counter = 0
+    errorCount = 0
+    fileExtensions = imageExtensions
+    if(mediaType == MediaType.VIDEO):
+        fileExtensions = videoExtensions
+    for fileName in srcFiles:
+        fullFileName = os.path.join(srcdir, fileName)
+        if (not os.path.isdir(fullFileName)):
+            if any(s in fileName for s in fileExtensions) and not fileName.startswith('.'):
+                try:
+                    counter = counter + 1
+                    creationDate = getCreationDate(fullFileName)
+                    moveResult = createDateFoldersAndMoveMedia(
+                        creationDate, fileName, fullFileName)
+                except Exception as e:
+                    errorCount = errorCount + 1
+                    #print("Error copying file " + fileName + " with error " + e)
+        if (counter == 20000):
+            break
+    print("Total number of errors: " + str(errorCount))
 
 
-organizeImagesByDateFolders(
-	"F:/ALL_MEDIA")
+organizeMediaByDateFolders("F:/ALL_MEDIA", MediaType.VIDEO)
