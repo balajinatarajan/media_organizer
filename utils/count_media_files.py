b7d2950e1c2264138b48dee9fb4a967757ecb6e6
Balaji Natarajan <code.balaji@gmail.com>
2020-05-08 20:55:54 -0400

    Adding fileSize filter

--- utils/count_media_files.py
+++ utils/count_media_files.py
@@ -4,10 +4,11 @@ import shutil
 imageExtensions = [".JPG", ".jpg", ".jpeg", ".JPEG", ".png", ".PNG"]
 videoExtensions = [".mp4", ".MP4", ".mov", ".MOV", ".m4v", ".M4V"]
 
-fileSizeLowerLimit = 200
-
+fileSizeLowerLimit = 0
+errorCount = 0
 
 def countFilesRecursively(srcdir, count):
+    global errorCount
     srcFiles = os.listdir(srcdir)
     for fileName in srcFiles:
         fullFileName = os.path.join(srcdir, fileName)
@@ -16,12 +17,16 @@ def countFilesRecursively(srcdir, count):
                 fullFileName, count)
         else:
             if not fileName.startswith('.') and (any(s in fileName for s in imageExtensions) or any(s in fileName for s in videoExtensions)):
-                fileSize = os.stat(fileName).st_size
-                if(fileSize > fileSizeLowerLimit):
-                    count = count + 1
+                try:
+                    fileSize = os.stat(fullFileName).st_size
+                    if(fileSize > fileSizeLowerLimit):
+                        count = count + 1
+                except:
+                    errorCount = errorCount + 1
     return count
 
 
-src = "F:/California and unsorted pictures"
+src = "F:/Unsorted"
 count = countFilesRecursively(src, 0)
 print("Total media: " + str(count))
+print("Total errors: " + str(errorCount))
