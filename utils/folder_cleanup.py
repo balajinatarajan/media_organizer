01ae0a14d9bd815faeef0c9087b76163e1b31fe7
Bala Natarajan <code.balaji@gmail.com>
2020-05-20 21:09:25 -0400

    Utility to delete empty dirs recursively

---
+++ utils/folder_cleanup.py
@@ -0,0 +1,19 @@
+import os
+
+
+def deleteEmptyDirsRecursively(dirName):
+    listOfEmptyDirs = list()
+
+    listOfEmptyDirs = [dirpath for (dirpath, dirnames, filenames) in os.walk(
+        dirName) if len(dirnames) == 0 and len(filenames) == 0]
+
+    if len(listOfEmptyDirs) != 0:
+        for elem in listOfEmptyDirs:
+            print("Deleting folder " + elem)
+            os.rmdir(elem)
+        deleteEmptyDirsRecursively(dirName)
+    return
+
+
+if __name__ == '__main__':
+    deleteEmptyDirsRecursively('/Users/bala/tmp')
